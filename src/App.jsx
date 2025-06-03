fe4a1f09e698415ff2edab207a5f261e7023fb41
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-06-04 02:33:11 +0400

    added genre validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,56 @@ import Card from './components/Card';
 
 function App() {
   const [data, setData] = useState([]);
+  const [selectedSubject, setSubject] = useState('history')
 
-  
 
+  let subject = [
+    'art',
+    'biography',
+    'children',
+    'fantasy',
+    'health',
+    'history',
+    'mystery',
+    'philosophy',
+    'religion',
+    'science',
+    'technology',
+  ]
 
   useEffect(() => {
-    fetch("https://openlibrary.org/subjects/history.json?limit=15")
+    fetch(`https://openlibrary.org/subjects/${selectedSubject}.json?limit=15`)
       .then(res => res.json())
       .then(res => setData(res.works));
-  }, []);
+  }, [selectedSubject]);
 
-  
+
+
+
 
   return (
     <div className="App">
-      <h1 className='text-center text-[50px]'>Library Books</h1>
+
+      <div className='max-w-[1440px] m-auto p-3 mt-5'>
+        <h2 className='text-center text-[50px]'>Book genres</h2>
+        <div className='flex gap-[20px] justify-center items-center mt-[60px]'>
+          {subject.map((e) => {
+            return (
+              <button onClick={() => setSubject(e)} className={`btn ${selectedSubject === e ? `activeBtn` : ''}`} >{e}</button>
+            )
+          })}
+        </div>
+      </div>
+
+      <h1 className='text-center text-[50px] mt-5'>Library Books</h1>
       <div className="max-w-[1440px] m-auto p-3 mt-5" >
-        <div className='flex flex-wrap justify-center items-center gap-[20px] text-start'>
+        <div className='flex flex-wrap justify-center items-center gap-[20px] text-start mt-[60px]'>
           {data.map((card) => (
-            <Card key={card.id}
+            <Card key={card.key}
               title={card.title}
               author={card.authors}
               editionCount={card.edition_count}
-              cover_id={card.cover_id  || 'No cover'} />
+              cover_id={card.cover_id} />
           ))}
         </div>
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 function Card({ title, author, editionCount, cover_id }) {
 
+    const noImg = `/nocover.jpg` 
+
     const imageUrl = cover_id
         ? `https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`
-        : 'https://via.placeholder.com/150';
+        : noImg;
 
     return (
         <div className='cursor-pointer'>
-            <img className="mb-2 w-full h-64 object-cover rounded" src={imageUrl} alt="" />
-            <h2 className='font-bold '>{author.map((e) => e.name)}</h2>
-            <p>{title}</p>
+            <img className="mb-2 w-full h-64 object-contain rounded" src={imageUrl} alt="" />
+            <h2 className='font-bold '>{author[0]?.name }</h2>
+            <p className='max-w-[300px]'>{title}</p>
             <p>
                 <span className='font-bold'>Edition count:</span > {editionCount}
             </p>
